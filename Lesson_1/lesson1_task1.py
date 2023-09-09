94d1f1016a9477a5f159cf356394cc0b75124dc1
PavelSB <1987PavelS@mail.ru>
2023-09-09 12:03:59 +0300

    lesson 1 task 1 changed

--- Lesson_1/lesson1_task1.py
+++ Lesson_1/lesson1_task1.py
@@ -1,3 +1,14 @@
 # Напишите простое веб-приложение на Flask, которое будет выводить на экран текст "Hello, World!".
 
+from flask import Flask
 
+app = Flask(name)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if name == 'main':
+    app.run()
